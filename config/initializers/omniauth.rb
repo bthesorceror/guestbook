9fd5b80749854163ee6ed9d822a19f480756305b
Brandon Farmer <bthesorceror@gmail.com>
2012-05-25 19:59:19 -0400

    BUGFIX: config yaml not opened for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
-TWITTER_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/twitter.yml")
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   else
+    TWITTER_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/twitter.yml")
     provider :twitter, TWITTER_CONFIG['twitter_key'], TWITTER_CONFIG['twitter_secret']
   end
 end
